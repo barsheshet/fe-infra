d8a4709616bd65756115ea05e9a9a5d817edce9c
Guy Barsheshet <guy@barsheshet.com>
2020-08-03 10:24:19 +0300

    Added Ant Design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <>
-    <nav className="navbar fixed-top navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">fe-infra</a>
-    </nav>
-    </>
+    <NavBar />
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export default function NavBar (props) {
+  return (<Button type="primary">Button</Button>);
+}
