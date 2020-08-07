9547f8b37a0eaff92d8740195228df933b56a044
Guy Barsheshet <guy@barsheshet.com>
2020-08-07 12:13:26 +0300

    State management setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Button } from 'antd';
+import { UserContext } from '../context/user-context';
 
 export default function NavBar (props) {
-  return (<Button type="primary">Button</Button>);
+  const [state, dispach] = useContext(UserContext);
+
+  useEffect(() => {
+    setTimeout(() => dispach({ type: 'SET_JWT', jwt: 'foo'}), 5000);
+  });
+
+  return (<Button type="primary">{state.jwt}</Button>);
 }
